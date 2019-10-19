fa3dca40b39c7cebc859e0361a7223432a48ac17
Rosyuku <sogenakotoga@homail.com>
2019-10-20 02:47:51 +0900

    logging用のスクリプトを追加

--- RosyukuScript.py
+++ RosyukuScript.py
@@ -1,4 +1,48 @@
 # -*- coding: utf-8 -*-
+import logging
+def getLogger(name, level=logging.DEBUG, saveName="noname.log"):
+    """
+    Loggerを作成する。
+    name：Loggerの名前（string)
+    level:Loggingのレベル(int)
+    saveName：Loggerの保存先（string)
+    """
+    #ロガーの定義
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    #フォーマットの定義
+    formatter = logging.Formatter("%(asctime)s:%(name)s:%(levelname)s:%(message)s")
+    #ファイル書き込み用
+    fh = logging.FileHandler(saveName)
+    fh.setFormatter(formatter)
+    #コンソール出力用
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    #それぞれロガーに追加
+    logger.addHandler(fh)
+    logger.addHandler(sh)
+    return logger
+
+def killLogger(logger):
+   """
+   loggerを削除する
+   logger：削除したいロガー(logging.Logger)
+   """
+   name = logger.name
+   logging.Logger.manager.loggerDict[name]
+   
+   return
+
+def killhandler(logger, handles):
+   """
+   loggerから特定のハンドルを削除する
+   logger：ハンドルを削除したいロガー(logging.Logger)
+   handles：削除したいハンドル(list)
+   """
+   for handle in handles:
+       logger.removeHandler(handle)
+       
+   return
  
 def pan2sk(df, target, name="Data"):
     """
